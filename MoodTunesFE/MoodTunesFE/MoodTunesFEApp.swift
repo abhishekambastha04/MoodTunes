f48c70411c96cb83488c16428d01855b10e5391b
Abhishek Ambastha <abhishekambastha@Abhisheks-MBP.lan>
2024-08-30 18:10:23 -0500

    Adds initial skeleton for swift frontend

---
+++ MoodTunesFE/MoodTunesFE/MoodTunesFEApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTunesFEApp.swift
+//  MoodTunesFE
+//
+//  Created by Abhishek Ambastha on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MoodTunesFEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
