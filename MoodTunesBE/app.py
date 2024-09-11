9a86466943af04edddf5c0bebf06a2c51b0cde8b
Abhishek Ambastha <abhishekambastha@Abhisheks-MBP.hsd1.ca.comcast.net>
2024-09-10 17:04:40 -0700

    sets up a user pool with cognito

--- MoodTunesBE/app.py
+++ MoodTunesBE/app.py
@@ -1,10 +1,43 @@
-from flask import Flask
+import os
+import boto3
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+cognito_client = boto3.client('cognito-idp', region_name='us-east-1')
+load_dotenv()
+
+COGNITO_USER_POOL_ID = os.getenv('COGNITO_USER_POOL_ID')
+COGNITO_APP_CLIENT_ID = os.getenv('COGNITO_APP_CLIENT_ID')
 
 @app.route('/')
 def hello():
     return "Hello, World!"
 
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data['email']
+    password = data['password']
+    
+    try:
+        response = cognito_client.initiate_auth(
+            ClientId=COGNITO_APP_CLIENT_ID,
+            AuthFlow='USER_PASSWORD_AUTH',
+            AuthParameters={
+                'USERNAME': email,
+                'PASSWORD': password
+            }
+        )
+        return jsonify({
+            "message": "Login successful",
+            "tokens": response['AuthenticationResult']
+        }), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001)
+    
