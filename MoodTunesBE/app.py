e1800102e217ff3520daa28256048d6d4f0982f4
Abhishek Ambastha <abhishekambastha@Abhisheks-MBP.hsd1.ca.comcast.net>
2024-09-10 17:58:28 -0700

    sets up sign up and confirm code routes

--- MoodTunesBE/app.py
+++ MoodTunesBE/app.py
@@ -18,6 +18,7 @@ def hello():
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
+    
     email = data['email']
     password = data['password']
     
@@ -37,6 +38,44 @@ def login():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    email = data['email']
+    password = data['password']
+    
+    try:
+        response = cognito_client.sign_up(
+            ClientId=COGNITO_APP_CLIENT_ID,
+            Username=email,
+            Password=password,
+            UserAttributes=[
+                {
+                    'Name': 'email',
+                    'Value': email
+                },
+            ]
+        )
+        return jsonify({"message": "User signed up successfully, check your email for verification."}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    
+@app.route('/confirm-signup', methods=['POST'])
+def confirm_signup():
+    data = request.get_json()
+    email = data['email']
+    confirmation_code = data['confirmation_code']
+    
+    try:
+        response = cognito_client.confirm_sign_up(
+            ClientId=COGNITO_APP_CLIENT_ID,
+            Username=email,
+            ConfirmationCode=confirmation_code
+        )
+        return jsonify({"message": "User email confirmed successfully."}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001)
